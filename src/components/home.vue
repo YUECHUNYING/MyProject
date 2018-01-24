da1f9af6d431d412059c7b912dea8ea56751323d
YUECHUNYING <961703455@qq.com>
2018-01-24 16:19:33 +0800

    增加一个组件home.vue

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    歌曲{{song}} 歌手{{singer}}
+  </div>
+</template>
+<script>
+export default {
+  name: '小月月--五环',
+  data () {
+    return {
+      song: '等你下课',
+      singer: '周杰伦杨瑞代'
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  color: red;
+}
+</style>
